cb4f5c91b12f7c4c642ed1a236c0f50ffdfc69f1
LimeOrangePie <ji5489@gmail.com>
2021-06-16 02:59:38 +0900

    Enhance structures

--- prepare/prepare-paperaware.py
+++ prepare/prepare-paperaware.py
@@ -87,182 +87,33 @@ class ImageNet(torch.utils.data.Dataset):
         return image, label
 
 
-class SqueezeExciteModule(nn.Module):
-    def __init__(self, expand_size):
-        super(SqueezeExciteModule, self).__init__()
+class MobileNetV3(torch.nn.Module):
+    def __init__(self, width_mult, classifier, num_features):
+        self.width_mult = width_mult
+        self.classifier = classifier
+        self.num_features = num_features
 
-        self.se_0_0 = nn.AdaptiveAvgPool2d(output_size=1)
-        self.se_0_1 = nn.Flatten()
+        layers = [
 
-        self.se_1_0 = nn.Linear(in_features=expand_size, out_features=expand_size)
-        self.se_1_1 = nn.ReLU(inplace=True)
+        ]
 
-        self.se_2_0 = nn.Linear(in_features=expand_size, out_features=expand_size)
-        self.se_2_1 = nn.Hardsigmoid(inplace=True)
-
-    def forward(self, x):
-        x = self.se_0_0(x)
-        x = self.se_0_1(x)
-
-        x = self.se_1_0(x)
-        x = self.se_1_1(x)
-
-        x = self.se_2_0(x)
-        x = self.se_2_1(x)
-        x = torch.unsqueeze(x, -1)
-        x = torch.unsqueeze(x, -1)
-        return x
-
-
-class Bottleneck(nn.Module):
-    def __init__(self, in_channels, out_channels, dw_kernel_size, expand_size, squeeze_excite,
-                 nonlinearity, stride):
-        super(Bottleneck, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.expand_size = expand_size
-        self.squeeze_excite = squeeze_excite
-        self.stride = stride
-        self.dw_kernel_size = dw_kernel_size
-
-        if nonlinearity == 'hardswish':
-            self.Nonliearity = nn.Hardswish
-        elif nonlinearity == 'relu':
-            self.Nonliearity = nn.ReLU
-        else:
-            raise RuntimeError("No such nonlinearity!")
-
-        # 1x1 Conv2d + NL
-        self.bottleneck_0_0 = nn.Conv2d(in_channels=in_channels, out_channels=expand_size, kernel_size=(1, 1),
-                                        bias=False)
-        self.bottleneck_0_1 = nn.BatchNorm2d(num_features=expand_size)
-        self.bottleneck_0_2 = self.Nonliearity(inplace=True)
-
-        # Dwise + NL
-        self.bottleneck_1_0 = nn.Conv2d(in_channels=expand_size, out_channels=expand_size,
-                                        kernel_size=self.dw_kernel_size,
-                                        stride=self.stride, padding=self.dw_kernel_size[0] // 2, groups=expand_size,
-                                        bias=False)
-        self.bottleneck_1_1 = nn.BatchNorm2d(num_features=expand_size)
-
-        # Squeeze-Excite
-        if self.squeeze_excite:
-            self.squeeze_excite_0 = SqueezeExciteModule(
-                expand_size=expand_size
-            )
-        else:
-            self.squeeze_excite_0 = nn.Identity()
-
-        # Final 1x1 Conv2d
-        self.bottleneck_final_0 = nn.Conv2d(in_channels=expand_size, out_channels=out_channels, kernel_size=(1, 1),
-                                            bias=False)
-        self.bottleneck_final_1 = nn.BatchNorm2d(num_features=out_channels)
-
-        # Downsampling first layer
-        self.bottleneck_final_2 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                                            kernel_size=(1, 1), stride=self.stride, bias=False)
-
-    def forward(self, x):
-        x_0 = self.bottleneck_0_0(x)
-        x_0 = self.bottleneck_0_1(x_0)
-        x_0 = self.bottleneck_0_2(x_0)
-
-        x_0 = self.bottleneck_1_0(x_0)
-        x_0 = self.bottleneck_1_1(x_0)
-
-        x_1 = self.squeeze_excite_0(x_0)
-        x_0 = x_0 * x_1
-
-        x_0 = self.bottleneck_final_0(x_0)
-        x_0 = self.bottleneck_final_1(x_0)
-        x_b = self.bottleneck_final_2(x)
-        return x_0.add(x_b)
-
-
-class MobileNetV3(nn.Module):
-    def __init__(self, width_mult=1.0, classifier=True, classifier_out_features=1000):
-        super(MobileNetV3, self).__init__()
-
-        conv_0_0 = nn.Conv2d(in_channels=3, out_channels=int(16 * width_mult),
-                             kernel_size=(3, 3), stride=2, padding=3 // 2, bias=False)
-        conv_0_1 = nn.BatchNorm2d(num_features=int(16 * width_mult))
-        conv_0_2 = nn.Hardswish(inplace=True)
-
-        conv_1_0 = Bottleneck(in_channels=int(16 * width_mult),
-                              out_channels=int(16 * width_mult), dw_kernel_size=(3, 3),
-                              expand_size=16, squeeze_excite=True, nonlinearity='relu', stride=2)
-        conv_2_0 = Bottleneck(in_channels=int(16 * width_mult),
-                              out_channels=int(24 * width_mult), dw_kernel_size=(3, 3),
-                              expand_size=72, squeeze_excite=False, nonlinearity='relu', stride=2)
-        conv_3_0 = Bottleneck(in_channels=int(24 * width_mult),
-                              out_channels=int(24 * width_mult), dw_kernel_size=(3, 3),
-                              expand_size=88, squeeze_excite=False, nonlinearity='relu', stride=1)
-        conv_4_0 = Bottleneck(in_channels=int(24 * width_mult),
-                              out_channels=int(40 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=96, squeeze_excite=True, nonlinearity='hardswish', stride=2)
-        conv_5_0 = Bottleneck(in_channels=int(40 * width_mult),
-                              out_channels=int(40 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=240, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-        conv_6_0 = Bottleneck(in_channels=int(40 * width_mult),
-                              out_channels=int(40 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=240, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-        conv_7_0 = Bottleneck(in_channels=int(40 * width_mult),
-                              out_channels=int(48 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=120, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-        conv_8_0 = Bottleneck(in_channels=int(48 * width_mult),
-                              out_channels=int(48 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=144, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-        conv_9_0 = Bottleneck(in_channels=int(48 * width_mult),
-                              out_channels=int(96 * width_mult), dw_kernel_size=(5, 5),
-                              expand_size=288, squeeze_excite=True, nonlinearity='hardswish', stride=2)
-        conv_10_0 = Bottleneck(in_channels=int(96 * width_mult),
-                               out_channels=int(96 * width_mult), dw_kernel_size=(5, 5),
-                               expand_size=576, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-        conv_11_0 = Bottleneck(in_channels=int(96 * width_mult),
-                               out_channels=int(96 * width_mult), dw_kernel_size=(5, 5),
-                               expand_size=576, squeeze_excite=True, nonlinearity='hardswish', stride=1)
-
-        conv_12_0 = nn.Conv2d(in_channels=int(96 * width_mult), out_channels=int(576 * width_mult),
-                              kernel_size=(1, 1), bias=False)
-        conv_12_1 = nn.Hardswish(inplace=True)
-        conv_12_2 = nn.BatchNorm2d(num_features=int(576 * width_mult))
-
-        self.features = nn.Sequential(
-            conv_0_0,
-            conv_0_1,
-            conv_0_2,
-            conv_1_0,
-            conv_2_0,
-            conv_3_0,
-            conv_4_0,
-            conv_5_0,
-            conv_6_0,
-            conv_7_0,
-            conv_8_0,
-            conv_9_0,
-            conv_10_0,
-            conv_11_0,
-            conv_12_0,
-            conv_12_1,
-            conv_12_2
+        self.features = nn.Sequential(*layers)
+        self.conv = nn.Identity()
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Sequential(
+            nn.Linear(960, 1280),
+            nn.Hardswish(),
+            nn.Dropout(0.2),
+            nn.Linear(1280, num_features)
         )
-
-        if classifier:
-            self.classifiers = nn.Sequential(
-                nn.AdaptiveAvgPool2d(output_size=1),
-                nn.Flatten(start_dim=1),
-                nn.Linear(int(576 * width_mult), int(1024 * width_mult)),
-                nn.Dropout(p=0.2),
-                nn.Linear(int(1024 * width_mult), classifier_out_features)
-            )
-        else:
-            self.classifiers = nn.Identity()
 
     def forward(self, x):
         x = self.features(x)
-        x = self.classifiers(x)
+        x = self.conv(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
         return x
-
 
 
 class VOC(Dataset):
@@ -328,6 +179,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', '-e', default=200, type=int, help='Epochs (default: 200)')
     parser.add_argument('--learning-rate', '-l', default=0.005, type=float, help='Learning rate (default: 0.005)')
     parser.add_argument('--batch-size', '-b', default=384, type=int, help='Batch size (default: 384)')
+    parser.add_argument('--optimizer', '-o', default='adam', type=str, help='Optimizers (default: adam)')
     parser.add_argument('--num-workers', '-p', default=0, type=int, help='num_workers (default: 0)')
     parser.add_argument('--seed', '-s', default=None, type=int, help='Use deterministic algorithms and give static seeds (default: None)')
     parser.add_argument('--continue-weight', '-c', default=None, type=str, help='load weight and continue training')
@@ -357,35 +209,53 @@ if __name__ == '__main__':
     model = MobileNetV3(width_mult=1.0, classifier=True, classifier_out_features=1000).float().to(device)
     # distributed model
     # model = torch.nn.DataParallel(model)
-    # torchsummary.summary(model, input_size=(3, 224, 224), device=device.type)
+    torchsummary.summary(model, input_size=(3, 224, 224), device=device.type)
+    exit(0)
 
     labels = LabelReader(label_file_path='imagenet_label.list').load_label()
-    datasets = ImageNet(
+    train_dataset = ImageNet(
         labels=labels,
-        root_dir='/dataset/Data/CLS-LOC/train',
+        root_dir=r'C:\Dataset\ILSVRC\Data\CLS-LOC\train',
         transform=transforms.Compose([
             transforms.RandomResizedCrop((224, 224)),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
+                mean=[0.4547857, 0.4349471, 0.40525291],
+                std=[0.12003352, 0.12323549, 0.1392444]
             )
         ])
     )
 
-    validset_items = int(len(datasets) * 0.1)
-    trainset_items = len(datasets) - validset_items
-    train_dataset, valid_dataset = torch.utils.data.random_split(
-        datasets, [trainset_items, validset_items]
+    valid_dataset = ImageNet(
+        labels=labels,
+        root_dir=r'C:\Dataset\ILSVRC\Data\CLS-LOC\val-sub',
+        transform=transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.4547857, 0.4349471, 0.40525291],
+                std=[0.12003352, 0.12323549, 0.1392444]
+            )
+        ])
     )
 
+    batch_size = args.batch_size
     dataloader_extras = {'shuffle': True, 'num_workers': args.num_workers, 'pin_memory': True, 'drop_last': False}
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, **dataloader_extras)
-    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=max(1, int(args.batch_size / 3)), **dataloader_extras)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, **dataloader_extras)
+    valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, **dataloader_extras)
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate, weight_decay=1e-5, momentum=0.9)
+
+    print("INFO: using %s optimizer" % args.optimizer)
+    if args.optimizer.lower() == 'rmsprop':
+        optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate, weight_decay=1e-5, momentum=0.9)
+    elif args.optimizer.lower() == 'adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    else:
+        print("ERROR: specified %s optimizer not implemented" % args.optimizer)
+        exit(-1)
+
     ema = ExponentialMovingAverage(model.parameters(), decay=0.9999)
 
     total_epochs = args.epochs
@@ -419,7 +289,7 @@ if __name__ == '__main__':
     else:
         epoch_range = range(total_epochs)
 
-    # scaler = torch.cuda.amp.GradScaler()
+    scaler = torch.cuda.amp.GradScaler()
 
     epoch_val_accuracies = []
     exit_reason = 0
@@ -430,22 +300,23 @@ if __name__ == '__main__':
 
         accuracies = []
         losses = []
-        model.train(True)
+        model.train()
         for i, (image, label) in tr:
-            # with torch.cuda.amp.autocast():
-            image = image.cuda(device, non_blocking=True)
-            label = label.cuda(device, non_blocking=True)
+            with torch.cuda.amp.autocast():
+                image = image.cuda(device, non_blocking=True)
+                label = label.cuda(device, non_blocking=True)
 
-            output = model(image)
-            loss = criterion(output, label)
+                output = model(image)
+                loss = criterion(output, label)
 
-            loss.backward()
-            optimizer.step()
-            # scaler.scale(loss).backward()
-            # scaler.step(optimizer)
-            # scaler.update()
+                # loss.backward()
+                # optimizer.step()
+                loss = scaler.scale(loss)
+                loss.backward()
+                scaler.step(optimizer)
+                scaler.update()
+
             optimizer.zero_grad(set_to_none=True)
-
             ema.update(model.parameters())
 
             with torch.no_grad():
@@ -477,7 +348,7 @@ if __name__ == '__main__':
 
         accuracies = []
         losses = []
-        model.train(False)
+        model.eval()
         ema.store(model.parameters())
         ema.copy_to(model.parameters())
         for i, (image, label) in vl:
